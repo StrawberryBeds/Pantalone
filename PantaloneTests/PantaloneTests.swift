0778fdd3360e08f1aed274b1c74518261254c6d0
StrawberryBeds <samuel.l.wood@gmail.com>
2026-01-16 15:45:10 -0500

    Initial Commit

---
+++ PantaloneTests/PantaloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  PantaloneTests.swift
+//  PantaloneTests
+//
+//  Created by Samuel Wood on 2026-01-16.
+//
+
+import Testing
+@testable import Pantalone
+
+struct PantaloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
