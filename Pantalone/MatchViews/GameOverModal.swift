88944b5d57995a52b33bc2d73a7799c7fa5286fe
StrawberryBeds <samuel.l.wood@gmail.com>
2026-02-09 10:13:46 -0500

    Adds basic gameOverModal.

---
+++ Pantalone/MatchViews/GameOverModal.swift
@@ -0,0 +1,18 @@
+//
+//  GameOverModal.swift
+//  Pantalone
+//
+//  Created by Samuel Wood on 2026-02-09.
+//
+
+import SwiftUI
+
+struct GameOverModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameOverModal()
+}
