ada7618e46d301c2f0ebe3dee263be30368b3e56
StrawberryBeds <samuel.l.wood@gmail.com>
2026-03-03 17:49:58 -0500

    Replaces Slide Game Over alert with SlideGameOverModal

---
+++ Pantalone/GameViews/SlideGameOverModal.swift
@@ -0,0 +1,122 @@
+//
+//  MatchGameOverModal 2.swift
+//  Pantalone
+//
+//  Created by Samuel Wood on 2026-03-01.
+//
+
+
+//
+//  MatchGameOverModal.swift
+//  Pantalone
+//
+//  Created by Samuel Wood on 2026-02-09.
+//
+
+import SwiftUI
+
+struct MatchGameOverModal: View {
+    
+    @ObservedObject var menuViewModel: MenuViewModel
+//    @ObservedObject var sharingViewModel: SharingViewModel
+    
+    var gameLogic = GameLogic()
+    let customTitle = Font.custom("FrederickatheGreat-Regular", size: 36)
+    let cream = Color("Cream")
+    let pantalonePink = Color("PantalonePink")
+
+    
+    var body: some View {
+        
+        ZStack {
+            Rectangle()
+                .foregroundStyle(cream)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Congratulations!")
+                    .font(customTitle)
+                    .foregroundColor(.black)
+                    .bold()
+                    .padding()
+                
+                Image(menuViewModel.selectedCardSet?.setImage ?? "card_back_bird")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(16)
+                
+                
+                Text("You matched all the cards in \(gameLogic.turns) turns!")
+                    .bold()
+                    .foregroundColor(.black)
+                    .padding(10)
+                
+                if gameLogic.turns < 16 {
+                    Image(systemName: "trophy.fill")
+                        .padding(10)
+                        
+                    Text("Well done! That's the least turns possible!")
+                }
+                
+                else {
+                    
+                    Text("Try to match the cards in less than 16 turns to win a trophy.")
+                        .bold()
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .padding(10)
+                }
+                
+                
+//                HStack {
+//                    Button("Admire your game!") {
+//                        gameLogic.gameOverModalIsPresented = false
+//                    }
+//                    .buttonStyle(.borderedProminent)
+//                    .foregroundColor(colorScheme == .dark ? .black : .white)
+//                    .padding()
+//                    
+//                    Button("Play a new game!") {
+//                        gameLogic.gameOverModalIsPresented = false
+//                        gameLogic.handleReset()
+//                    }
+//                    .buttonStyle(.borderedProminent)
+//                    .foregroundColor(colorScheme == .dark ? .black : .white)
+//                    .padding()
+//                }
+                
+                HStack(spacing: 20) {
+                    Button(action: {
+                        gameLogic.gameOverModalIsPresented = false
+                    }) {
+                        Text("Admire your game!")
+                            .buttonStyle(.borderedProminent)
+                            .foregroundColor(.black)
+                            .frame(width: 132, height: 60)
+                            .background(.pantalonePink)
+                            .cornerRadius(30)
+                            .padding()
+                    }
+                    
+                    Button(action: {
+                        gameLogic.gameOverModalIsPresented = false
+                        gameLogic.handleReset()
+                    }) {
+                        Text("Play a new game!")
+                            .buttonStyle(.borderedProminent)
+                            .foregroundColor(.black)
+                            .frame(width: 132, height: 60)
+                            .background(.pantalonePink)
+                            .cornerRadius(30)
+                            .padding()
+                    }
+                }
+            }
+        }
+    }
+}
+
+//#Preview {
+//    GameOverModal(menuViewModel: menuViewModel, gameLogic: gameLogic)
+//}
